61bf3ea58924823d80c33003800c3401b95e91bc
David Allen <trooper898@gmail.com>
2020-06-08 19:39:34 -0400

    removed the JS and CSS folders, created basic HTML homepage, added comments for routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
-// add our routes
+// TODO: route for saving activity data
+
+// TODO: route for getting activity data by duration
+
+// TODO: route for getting activity data by mileage
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 // add our routes
 
+// HTML route for displaying the homepage
+
 module.exports = router;
