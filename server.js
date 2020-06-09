ef104f1063d281da57f150c8fec2d35c7ee7e06d
David Allen <trooper898@gmail.com>
2020-06-08 21:18:39 -0400

    working graphs

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = 8080;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// middleware that is needed to use POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // added some comment
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
